47595b12117729abcaeb631bd9f159a46f46f30a
EthanB <ebonsignori@gmail.com>
2019-03-30 11:03:22 -0400

    javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,22 @@ var empRole = "";
 var startDate = "";
 var monthlyRate = "";
 
+$('#inputButton').on("click", function(event) {
+    event.preventDefault();
 
-empName = $('')
-empRole = $
+
+
+empName = $('#nameInput').val().trim();
+empRole =  $('#roleInput').val().trim();
+startDate =  $('#dateInput').val().trim();
+monthlyRate =  $('#rateInput').val().trim();
+
+    database.ref().push({
+    empName: empName,
+    empRole: empRole,
+    startDate: startDate,
+    monthlyRate: monthlyRate,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+});
