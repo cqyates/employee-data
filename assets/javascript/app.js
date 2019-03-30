6b33138fab0548bb8708b31c583f823084aae38f
Whitney Crawford <whitneylcrawford@gmail.com>
2019-03-30 11:11:33 -0400

    added firebase config

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
+var config = {
+    apiKey: "AIzaSyD9NkcqUfr1uS6oAUYba8RKvUOJ5POJx88",
+    authDomain: "first-project-d1d3a.firebaseapp.com",
+    databaseURL: "https://first-project-d1d3a.firebaseio.com",
+    projectId: "first-project-d1d3a",
+    storageBucket: "first-project-d1d3a.appspot.com",
+    messagingSenderId: "621902807967"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
 var empName = "";
 var empRole = "";
 var startDate = "";
