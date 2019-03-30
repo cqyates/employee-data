edb5be72f20db05379d95a489062042f73dba1d8
EthanB <ebonsignori@gmail.com>
2019-03-30 11:23:27 -0400

    listen for child added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyD9NkcqUfr1uS6oAUYba8RKvUOJ5POJx88",
     authDomain: "first-project-d1d3a.firebaseapp.com",
@@ -5,32 +6,43 @@ var config = {
     projectId: "first-project-d1d3a",
     storageBucket: "first-project-d1d3a.appspot.com",
     messagingSenderId: "621902807967"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
+// Declare Variables
 var database = firebase.database();
-
 var empName = "";
 var empRole = "";
 var startDate = "";
 var monthlyRate = "";
 
+// Listen for clicks on submit button
 $('#inputButton').on("click", function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-
-
-empName = $('#nameInput').val().trim();
-empRole =  $('#roleInput').val().trim();
-startDate =  $('#dateInput').val().trim();
-monthlyRate =  $('#rateInput').val().trim();
-
-    database.ref().push({
+  // Get text input from input fields
+  empName = $('#nameInput').val().trim();
+  empRole =  $('#roleInput').val().trim();
+  startDate =  $('#dateInput').val().trim();
+  monthlyRate =  $('#rateInput').val().trim();
+  
+  // Push that data to firebase as a new child
+  database.ref().push({
     empName: empName,
     empRole: empRole,
     startDate: startDate,
     monthlyRate: monthlyRate,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+  });
 
 });
+
+database.ref().on("child_added", function(childSnapshot) {
+  
+  console.log(childSnapshot.val().empName)
+  console.log(childSnapshot.val().empRole)
+  console.log(childSnapshot.val().startDate)
+  console.log(childSnapshot.val().monthlyRate)
+  console.log(childSnapshot.val().dateAdded)
+  
+})
